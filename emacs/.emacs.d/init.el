0964a7d5ca74f303ebd08a65fcb56d89d1b90e6f
Liam <itsliamegan@protonmail.com>
2020-03-02 19:51:40 -0500

    Remove unnecessary Emacs packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,17 +1,3 @@
 ;; All my configuration lives in a heavily documented org file. We
 ;; load this file and evaluate all the emacs-lisp code blocks.
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml flx yaml-mode use-package solarized-theme minitest markdown-mode helm-projectile flycheck fill-column-indicator evil-surround evil-magit evil-collection engine-mode emms elfeed dumb-jump diminish counsel-projectile chruby))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
