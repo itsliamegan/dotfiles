267d4bd1bb5e4a6ebb81344b2db86d56d7023c57
Liam Egan <liam@liamegan.com>
2020-04-24 23:59:10 -0400

    Minitest -> RSpec

--- emacs/.emacs.d/modes/ruby.el
+++ emacs/.emacs.d/modes/ruby.el
@@ -7,17 +7,15 @@
             (lambda ()
               (with-suppressed-message (chruby-use-corresponding)))))
 
-(defun test-with-minitest ()
-  (add-hook 'ruby-mode-hook
-            (lambda ()
-              (minitest-mode +1))))
+(defun configure-rspec-independently-from-emacs ()
+  (setq rspec-command-options ""))
 
-(defun scroll-to-first-error-after-running-tests ()
-  (add-hook 'minitest-compilation-mode-hook
+(defun scroll-to-first-error-after-running-specs ()
+  (add-hook 'rspec-compilation-mode-hook
             (lambda ()
               (setq compilation-scroll-output 'first-error))))
 
 (properly-indent-after-newlines)
 (use-correct-ruby-from-chruby)
-(test-with-minitest)
-(scroll-to-first-error-after-running-tests)
+(configure-rspec-independently-from-emacs)
+(scroll-to-first-error-after-running-specs)
