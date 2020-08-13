ef75a5c631d98885f8ae58c45ad46e143e590380
Liam Egan <liam@liamegan.com>
2020-08-12 21:53:18 -0400

    Restructure projectile config

--- emacs/.emacs.d/modes/projectile.el
+++ emacs/.emacs.d/modes/projectile.el
@@ -1,4 +1,15 @@
-(setq projectile-completion-system 'ivy
-      projectile-switch-project-action 'projectile-dired)
+(defun complete-with-ivy ()
+  (setq projectile-completion-system 'ivy))
+
+(defun open-dired-on-switch-project ()
+  (setq projectile-switch-project-action 'projectile-dired))
+
+(defun ignore-third-party-files ()
+  (add-to-list 'projectile-globally-ignored-directories "*node_modules")
+  (add-to-list 'projectile-globally-ignored-directories "vendor"))
 
 (projectile-mode +1)
+
+(complete-with-ivy)
+(open-dired-on-switch-project)
+(ignore-third-party-files)
