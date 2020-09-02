1d0b1ae6151be6f4da499b39c14ef17eabb22e73
Liam Egan <liam@liamegan.com>
2020-09-02 09:46:25 -0400

    Use improvded JS mode

--- emacs/.emacs.d/modes/javascript.el
+++ emacs/.emacs.d/modes/javascript.el
@@ -1,4 +1,13 @@
+(defun use-improved-js-mode ()
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+
 (defun indent-using-two-spaces-in-js ()
   (setq js-indent-level 2))
 
+(defun dont-show-error-for-missing-semicolons ()
+  (setq js2-strict-missing-semi-warning nil)
+  (setq js2-missing-semi-one-line-override nil))
+
+(use-improved-js-mode)
 (indent-using-two-spaces-in-js)
+(dont-show-error-for-missing-semicolons)
