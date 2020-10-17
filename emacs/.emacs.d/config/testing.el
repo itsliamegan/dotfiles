76de6d81f55d51abef4b1bcbcae79551905b27cf
Liam Egan <liam@liamegan.com>
2020-10-17 13:19:39 -0400

    Minitest -> Rspec

--- emacs/.emacs.d/config/testing.el
+++ emacs/.emacs.d/config/testing.el
@@ -1,19 +1,19 @@
 (defun test-project ()
   (interactive)
-  (cond ((eq major-mode 'ruby-mode) (minitest-verify-all))))
+  (cond ((eq major-mode 'ruby-mode) (rspec-verify-all))))
 
 (defun test-file ()
   (interactive)
-  (cond ((eq major-mode 'ruby-mode) (minitest-verify))))
+  (cond ((eq major-mode 'ruby-mode) (rspec-verify))))
 
 (defun test-single ()
   (interactive)
-  (cond ((eq major-mode 'ruby-mode) (minitest-verify-single))))
+  (cond ((eq major-mode 'ruby-mode) (rspec-verify-single))))
 
 (defun test-rerun ()
   (interactive)
-  (cond ((eq major-mode 'ruby-mode) (minitest-rerun))))
+  (cond ((eq major-mode 'ruby-mode) (rspec-rerun))))
 
 (defun test-toggle ()
   (interactive)
-  (cond ((eq major-mode 'ruby-mode) (minitest-toggle-spec-and-target))))
+  (cond ((eq major-mode 'ruby-mode) (rspec-toggle-spec-and-target))))
