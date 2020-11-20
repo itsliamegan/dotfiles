efab6853551d2d71fbb0a7011bca60de1a9abdd9
Liam Egan <liam@liamegan.com>
2020-11-20 10:44:25 -0500

    Restructure directories

--- install.sh
+++ install.sh
@@ -41,9 +41,29 @@ gem install \
   standard
 
 # Symlink config files.
-stow bash
-stow git
-stow node
-stow ruby
-stow tmux
-stow vim
+paths=(
+  ".bashrc"
+  ".bundle"
+  ".gitconfig"
+  ".gitignore"
+  ".node-version"
+  ".ruby-version"
+  ".tmux.conf"
+  ".vimrc"
+)
+for path in ${paths[@]}; do
+  local_path="$PWD/$path"
+  home_path="$HOME/$path"
+
+  if ! [[ -f $local_path || -d $local_path ]]; then
+    echo "$local_path does not exist"
+    continue
+  fi
+
+  if [[ -f $home_path || -d $home_path ]]; then
+    echo "$home_path already exists"
+    continue
+  fi
+
+  ln -s $local_path $home_path
+done
